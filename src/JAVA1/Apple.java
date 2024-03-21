ab17b99f5979575087b9353d8b00dd1825a3a91f
chldbf <c01058984647@gmail.com>
2024-03-21 15:49:49 +0900

    03/21

---
+++ src/JAVA1/Apple.java
@@ -0,0 +1,50 @@
+package JAVA1;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		
+//		자료형
+//		int(4) : 정수
+//		long(8) : 정수
+//
+//		float(4) : 실수
+//		double(8) : 실수
+//
+//		char(1) : 문자
+//		String : 문자열
+		
+		// 정수
+		int a = 1;
+		long l = 2;
+		
+		// 실수
+		float b = 3.14f;
+		double c = 2.15d;
+		
+		// 문자 & 문자열
+		char d = 'a';
+		String e = "bhbhybybg";
+		
+		int p = 1;
+		int o = 2;
+		int abc = 100;
+		
+		String Apple = "abc";
+		String korea = "Korea";
+		
+		char fff = 'C';
+		char file = 'f';
+		
+		long ll = 50;
+		
+		float time = 1.5f;
+		float pen = 2.5f;
+		
+		double dodd = 2.3d;
+		double ctrl = 3.3d;
+		
+		
+	}
+
+}
---
+++ src/JAVA1/ood.java
@@ -0,0 +1,23 @@
+package JAVA1;
+
+public class ood {
+
+	public static void main(String[] args) {
+		System.out.println("드디어ㅕ");
+
+		int a = 0;
+		String an = null;
+
+		for (int f = 1; f <= 10; f++) {
+			System.out.println(f);
+		}
+
+		while (a <= 10) {
+			System.out.println(a);
+			a++;
+		}
+
+		int[] c = new int[3];
+	}
+
+}
